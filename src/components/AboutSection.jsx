7c856f3eb6688683f3b534947cd50f2208ce155e
VaradCodes31 <varadalshi7@gmail.com>
2025-05-10 17:54:18 +0530

    first commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,6 @@ import { DollarSign, Code, User } from "lucide-react";
 export const AboutSection = () => {
   return (
     <section id="about" className="py-24 px-4 relative">
-      {" "}
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
           About <span className="text-primary"> Me</span>
@@ -31,12 +30,12 @@ export const AboutSection = () => {
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">
-                {" "}
                 Get In Touch
               </a>
 
               <a
-                href=""
+                href="/Varad_Main.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
@@ -51,7 +50,7 @@ export const AboutSection = () => {
                   <Code className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg"> Data Scientist</h4>
+                  <h4 className="font-semibold text-lg">Data Scientist</h4>
                   <p className="text-muted-foreground">
                     Focused on developing data-driven solutions using modern tools and frameworks, 
                     with growing proficiency shaped by practical projects and a commitment to continuous 
@@ -60,6 +59,7 @@ export const AboutSection = () => {
                 </div>
               </div>
             </div>
+
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
@@ -74,6 +74,7 @@ export const AboutSection = () => {
                 </div>
               </div>
             </div>
+
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
