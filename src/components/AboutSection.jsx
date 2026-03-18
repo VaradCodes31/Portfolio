0cb5bc28cd76649df366277f8dd46a381efa551b
Varad <admin@Admins-MacBook-Pro.local>
2026-03-18 16:04:32 +0530

    Updated Resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Varad_Main.pdf"
+                href="/Varad_New.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
